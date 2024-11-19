ddd638ab06051873eab2bcc8f8c686eca89dbd8c
Marcos150 <marcosruizrubio@gmail.com>
2024-11-19 11:18:33 +0100

    Cosas agregadas en la última clase

---
+++ testGen/WebApplication1/Assemblers/MascotaAssembler.cs
@@ -0,0 +1,18 @@
+﻿using TestGen.ApplicationCore.EN.DSM;
+using WebApplication1.Models;
+
+namespace WebApplication1.Assemblers
+{
+    public class MascotaAssembler
+    {
+        public MascotaViewModel ConvertirEnToModel(MascotaEN en)
+        {
+            MascotaViewModel model = new MascotaViewModel();
+
+            model.Id = en.Id;
+            model.Name = en.Nombre;
+
+            return model;
+        }
+    }
+}
---
+++ testGen/WebApplication1/Models/MascotaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class MascotaViewModel
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Display(Prompt ="Escribe el nombre de la mascota", Description="Nombre de la mascota", Name="nombre")]
+        [Required]
+        public string? Name { get; set; }
+    }
+}
