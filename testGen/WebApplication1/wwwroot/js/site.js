e4953f370edc918ad24c18b5c774f8e63b67da28
Marcos150 <marcosruizrubio@gmail.com>
2024-12-03 11:19:39 +0100

    Función javascript para mostrar mensajes modales

--- testGen/WebApplication1/wwwroot/js/site.js
+++ testGen/WebApplication1/wwwroot/js/site.js
@@ -1,4 +1,15 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿"use strict";
+// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Ejemplo de uso en boton: <button onclick="mostrarMensaje('Esto es el título', 'Esto es el mensaje', 'success');">Demo mensaje modal</button>
+//Icono puede ser "success", "error", "warning", "info" o "question"
+function mostrarMensaje(titulo, texto, icono = "info") {
+    Swal.fire({
+        title: titulo,
+        text: texto,
+        icon: icono
+    });
+}
