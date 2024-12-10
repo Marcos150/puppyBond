1ce946ae378c9077c1259d2a563dc67fe699cbb3
Marcos <marcosruizrubio@gmail.com>
2024-12-10 13:16:53 +0100

    Registro con validadores corregidos

--- testGen/WebApplication1/Models/UsuarioViewModel.cs
+++ testGen/WebApplication1/Models/UsuarioViewModel.cs
@@ -4,22 +4,33 @@ namespace WebApplication1.Models
 {
     public class UsuarioViewModel
     {
-        [ScaffoldColumn(false)]
-        public string? Email { get; set; }
+        [Display(Prompt = "Email", Description = "Email del usuario", Name = "Email")]
+        [Required(ErrorMessage = "El email es obligatorio")]
+        [EmailAddress(ErrorMessage = "El correo introducido no es válido")]
+        public string Email { get; set; }
 
-        [Display(Prompt = "Escribe el nombre del usuario", Description = "Nombre del usuario", Name = "Nombre")]
-        public string? Nombre { get; set; }
+        [Display(Prompt = "Nombre", Description = "Nombre del usuario", Name = "Nombre")]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
 
-        [Display(Prompt = "Escribe los apellidos del usuario", Description = "Apellidos del usuario", Name = "Apellidos")]
-        public string? Apellidos { get; set; }
+        [Display(Prompt = "Apellidos", Description = "Apellidos del usuario", Name = "Apellidos")]
+        [Required(ErrorMessage = "Los apellidos son obligatorios")]
+        public string Apellidos { get; set; }
 
-        [Display(Prompt = "Escribe la disponibilidad del usuario", Description = "Disponibilidad del usuario", Name = "Disponibilidad")]
-        public string? Disponibilidad { get; set; }
+        [Display(Prompt = "Disponibilidad", Description = "Disponibilidad del usuario", Name = "Disponibilidad")]
+        [Required(ErrorMessage = "La disponibilidad es obligatoria")]
+        public string Disponibilidad { get; set; }
 
-        [Display(Prompt = "Escribe la contraseña del usuario", Description = "Contraseña del usuario", Name = "Contraseña")]
-        public string? Pass { get; set; }
+        [Display(Prompt = "Contraseña", Description = "Contraseña del usuario", Name = "Contraseña")]
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "La contraseña debe tener al menos 8 caracteres")]
+        //TODO: Regex de comprobacion de numeros y caracteres especiales
+        public string Pass { get; set; }
 
-        [Display(Prompt = "Escribe la ubicación del usuario", Description = "Ubicación del usuario", Name = "Ubicación")]
-        public string? Ubicacion { get; set; }
+        //TODO: Pensar en un mejor metodo para obtener ubicacion
+        [Display(Prompt = "Ubicación", Description = "Ubicación del usuario", Name = "Ubicacion")]
+        [Required(ErrorMessage = "La ubicación es obligatoria")]
+        public string Ubicacion { get; set; }
     }
 }
